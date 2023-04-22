f956179ee9ca0a2350fec22d9a912f27d941ed26
oksunwoo <oksunwo@naver.com>
2023-04-22 19:30:05 +0900

    feat: SearchBar 추가

--- AppStore/AppStore/Presentation/Search/View/SearchViewController.swift
+++ AppStore/AppStore/Presentation/Search/View/SearchViewController.swift
@@ -8,16 +8,41 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-
+    private let searchField: UISearchBar = {
+        let searchbar = UISearchBar()
+        searchbar.translatesAutoresizingMaskIntoConstraints = false
+        searchbar.placeholder = "게임, 앱, 스토리 등"
+        searchbar.setImage(UIImage(systemName: "magnifyingglass"), for: .search, state: .normal)
+        searchbar.setImage(UIImage(systemName: "x.circle.fill"), for: .clear, state: .normal)
+        
+        return searchbar
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        configureBackground()
         configureNavigationBar()
+        configureLayout()
+    }
+    
+    private func configureBackground() {
+        view.backgroundColor = .systemBackground
     }
    
     private func configureNavigationBar() {
         self.navigationController?.navigationBar.prefersLargeTitles = true
         self.navigationController?.navigationBar.topItem?.title = "Search"
+    }
+    
+    private func configureLayout() {
+        view.addSubview(searchField)
+        
+        NSLayoutConstraint.activate([
+            searchField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            searchField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 12),
+            searchField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -12)
+        ])
     }
 }
 
