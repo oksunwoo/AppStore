5cdff3474325d3df6b4dcd3949efafa73ee63990
oksunwoo <oksunwo@naver.com>
2023-04-30 22:52:23 +0900

    feat: viewModel 작성

--- AppStore/AppStore/Presentation/Search/ViewModel/SearchViewModel.swift
+++ AppStore/AppStore/Presentation/Search/ViewModel/SearchViewModel.swift
@@ -6,7 +6,29 @@
 //
 
 import Foundation
+import Combine
 
-class SearchViewModel {
+class SearchViewModel: ViewModelPrototol {
+    struct Input {
+        let keyword: AnyPublisher<String, Never>
+    }
+    
+    struct Output {
+        let isAPISuccess: AnyPublisher<Bool, Never>
+    }
+    
+    private let coordinator: SearchCoordinator
+    
+    init(coordinator: SearchCoordinator) {
+        self.coordinator = coordinator
+    }
+    
+    func transform(input: Input) -> Output {
+    
+    }
+    
+    private func fetchData(with keyword: String) -> AnyPublisher<SearchResultDTO, NetworkError> {
+        return ItunesAPI.SearchAPI(appName: keyword).fetchData()
+    }
     
 }
